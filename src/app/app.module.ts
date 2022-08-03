f8777b1be6bd790ecba8926ade99b82af1337932
kliastro13 <it.goncharov@gmail.com>
2022-08-03 22:36:49 +0300

    Added examle component nz-button. Generated example component form-normal-login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,20 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { uk_UA } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import uk from '@angular/common/locales/uk';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { LoginComponent } from './components/login/login.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 registerLocaleData(uk);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,6 +30,9 @@ registerLocaleData(uk);
     IconsProviderModule,
     NzLayoutModule,
     NzMenuModule,
+    NzButtonModule,
+    ReactiveFormsModule,
+    NzFormModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: uk_UA }],
   bootstrap: [AppComponent],
