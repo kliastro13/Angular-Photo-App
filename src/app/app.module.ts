f2c0c3739b5e28debcb6fd409d68c918b99c2c88
kliastro13 <it.goncharov@gmail.com>
2022-08-06 23:15:49 +0300

    Added ng-zorro dependencies for form, button, input and checkbox.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzFormModule } from 'ng-zorro-antd/form';
 
 registerLocaleData(uk);
 
@@ -27,11 +23,7 @@ registerLocaleData(uk);
     HttpClientModule,
     BrowserAnimationsModule,
     IconsProviderModule,
-    NzLayoutModule,
-    NzMenuModule,
-    NzButtonModule,
     ReactiveFormsModule,
-    NzFormModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: uk_UA }],
   bootstrap: [AppComponent],
--- src/app/components/account/account.module.ts
+++ src/app/components/account/account.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzCheckboxModule,
+  ],
 })
-export class AccountModule { }
+export class AccountModule {}
