d824c004fb1ed761b4d2fc474c97a1bb548d4396
kliastro13 <it.goncharov@gmail.com>
2022-08-09 21:12:59 +0300

    Created login component.

--- src/app/components/account/account.module.ts
+++ src/app/components/account/account.module.ts
@@ -5,9 +5,12 @@ import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     NzFormModule,
