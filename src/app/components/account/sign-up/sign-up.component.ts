8886b0b7ab12fdd9a15ae2c2ddadfb447ccd7932
kliastro13 <it.goncharov@gmail.com>
2022-09-01 23:06:50 +0300

    Commit: #5 Create a sign-up page. - Added standard register component from ng zorro.

--- src/app/components/account/sign-up/sign-up.component.ts
+++ src/app/components/account/sign-up/sign-up.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Observable, Observer } from 'rxjs';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.less']
+  styleUrls: ['./sign-up.component.less'],
 })
 export class SignUpComponent implements OnInit {
+  validateForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
   }
 
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.validateForm.reset();
+    for (const key in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(key)) {
+        this.validateForm.controls[key].markAsPristine();
+        this.validateForm.controls[key].updateValueAndValidity();
+      }
+    }
+  }
+
+  validateConfirmPassword(): void {
+    setTimeout(() =>
+      this.validateForm.controls['confirm'].updateValueAndValidity()
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  userNameAsyncValidator = (control: FormControl) =>
+    new Observable((observer: Observer<ValidationErrors | null>) => {
+      setTimeout(() => {
+        if (control.value === 'JasonWood') {
+          // you have to return `{error: true}` to mark it as an error event
+          observer.next({ error: true, duplicated: true });
+        } else {
+          observer.next(null);
+        }
+        observer.complete();
+      }, 1000);
+    });
+
+  confirmValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { error: true, required: true };
+    } else if (control.value !== this.validateForm.controls['password'].value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  };
+
+  constructor(private fb: FormBuilder) {
+    this.validateForm = this.fb.group({
+      userName: ['', [Validators.required], [this.userNameAsyncValidator]],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]],
+      confirm: ['', [this.confirmValidator]],
+      comment: ['', [Validators.required]],
+    });
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
