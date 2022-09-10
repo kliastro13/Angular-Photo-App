1a702f0b1feae99993c6d64a396321bdd370ebee
kliastro13 <it.goncharov@gmail.com>
2022-09-10 21:29:05 +0300

    Commit: #9 Implement auth guard. - Added file const.ts for constants values; - Updated path for welcome component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/account/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AuthGuard } from './guards/auth/auth.guard';
+import { homepage } from './data/const';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   {
-    path: 'welcome',
+    path: homepage,
     loadChildren: () =>
       import('./components/welcome/welcome.module').then(
         (m) => m.WelcomeModule
--- src/app/components/welcome/welcome-routing.module.ts
+++ src/app/components/welcome/welcome-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from './welcome.component';
 
-const routes: Routes = [{ path: '/welcome', component: WelcomeComponent }];
+import { WelcomeComponent } from './welcome.component';
+import { homepage } from 'src/app/data/const';
+
+const routes: Routes = [{ path: homepage, component: WelcomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
