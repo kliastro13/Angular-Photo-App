024bb3499ab35b5ff35abc91c9f08bc109964558
kliastro13 <it.goncharov@gmail.com>
2023-03-29 13:28:29 +0300

    Commit: #16 Bug fix for sign up page. Added path to sign-up page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccountRoutingModule } from './components/account/account-routing.module';
 
 import { LoginComponent } from './components/account/login/login.component';
+import { SignUpComponent } from './components/account/sign-up/sign-up.component';
 import { AuthGuard } from './guards/auth/auth.guard';
 import { homepage } from './data/const';
 import { LoggedInGuard } from './guards/loggedIn/logged-in.guard';
@@ -10,6 +11,7 @@ import { LoggedInGuard } from './guards/loggedIn/logged-in.guard';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent, canActivate: [LoggedInGuard] },
+  { path: 'sign-up', component: SignUpComponent, canActivate: [LoggedInGuard] },
   {
     path: homepage,
     loadChildren: () =>
