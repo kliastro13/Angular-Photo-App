054bb241aa388ce14c184fbbbab7edd443ddfaf0
kliastro13 <it.goncharov@gmail.com>
2023-04-01 08:58:44 +0300

    Commit: #12 Implement auth service. Updated authButton with log out button.

--- src/app/components/account/login/authButtonComponent/authButton.component.ts
+++ src/app/components/account/login/authButtonComponent/authButton.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-
-// Import the AuthService type from the SDK
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-auth-button',
-  template: '<button (click)="auth.loginWithRedirect()">Log in</button>'
+  template: `
+    <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
+      <button (click)="auth.logout({ logoutParams: { returnTo: document.location.origin } })">
+        Log out
+      </button>
+    </ng-container>
+
+    <ng-template #loggedOut>
+      <button (click)="auth.loginWithRedirect()">Log in</button>
+    </ng-template>
+  `,
+  styles: [],
 })
 export class AuthButtonComponent {
-  // Inject the authentication service into your component through the constructor
-  constructor(public auth: AuthService) {}
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
 }
