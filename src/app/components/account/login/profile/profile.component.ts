153e67ed010819c59a6b18e88a84b289660a0cf4
kliastro13 <it.goncharov@gmail.com>
2023-05-15 22:59:50 +0300

    Commit: #17 Implement auth service. Deleted user service. Added working version of Auth Guard. Added indication of Authentication. Added firs variant of updated Login Guard.

--- src/app/components/account/login/profile/profile.component.ts
+++ src/app/components/account/login/profile/profile.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -10,6 +11,19 @@ import { AuthService } from '@auth0/auth0-angular';
       <li>{{ user.email }}</li>      
     </ul>`
 })
-export class UserProfileComponent {
-  constructor(public auth: AuthService) {}
-}
+export class UserProfileComponent implements OnInit{
+  constructor(public auth: AuthService) {}  
+
+  ngOnInit(): void {
+   this.auth.isAuthenticated$.subscribe(v => console.log(v))
+  }
+} 
+
+    
+
+  
+
+ 
+  
+ 
+  
--- src/app/guards/loggedIn/logged-in.guard.ts
+++ src/app/guards/loggedIn/logged-in.guard.ts
@@ -1,35 +1,68 @@
 import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
-  CanActivate,
   RouterStateSnapshot,
-  UrlTree,
+  CanActivate,  
+  Route,
+  UrlSegment,
+  CanActivateChild,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
-import { UserService } from 'src/app/services/userService/user.service';
 import { homepage } from 'src/app/data/const';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoggedInGuard implements CanActivate {
-  constructor(private userService: UserService, private router: Router) {}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+  constructor( private router: Router, private auth: AuthService) {}
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>{
     console.log('LoggedIn Guard working!');
-
-    if (!this.userService.isLoggedIn()) {
-      return true;
-    }
-    this.router.navigate([homepage]);
-    return false;
+    return this.redirectIfUnauthenticated(state);
   }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.redirectIfUnauthenticated(state);
+  }
+
+  private redirectIfUnauthenticated(
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.auth.isAuthenticated$.pipe(
+      tap((loggedIn) => {
+        if (loggedIn) {
+          this.auth.loginWithRedirect({
+            appState: { target: homepage },
+          });
+        }
+      })
+    );
+  }
+
+
 }
+
+/* if (!this.userService.isLoggedIn()) {
+  return true;
+}
+this.router.navigate([homepage]);
+return false; */
+
+
+/* 
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
+    return this.auth.isAuthenticated$.pipe(take(1));
+  } */
+
+
+
+
+
+
+
