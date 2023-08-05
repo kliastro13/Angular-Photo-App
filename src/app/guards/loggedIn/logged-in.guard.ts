5ab26af54f918ae8e7f8d08a4e9faf97446f639f
kliastro13 <it.goncharov@gmail.com>
2023-08-05 22:02:47 +0300

    Commit: #23 remove unnecessary components and change structure. Changed the purpose of the welcome component (should be displayed when user is not user login).

--- src/app/guards/loggedIn/logged-in.guard.ts
+++ src/app/guards/loggedIn/logged-in.guard.ts
@@ -10,18 +10,18 @@ import { homepage } from 'src/app/data/const';
 @Injectable({
   providedIn: 'root',
 })
-export class LoggedInGuard implements CanActivate {  
+export class LoggedInGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
   canActivate(): Observable<boolean>{
     console.log('LoggedIn Guard working!');
-    return this.isNotAuthenticated();    
+    return this.isNotAuthenticated();
   }
 
   private isNotAuthenticated(): Observable<boolean> {
     return this.auth.isAuthenticated$.pipe(
       map(v => {
         if(v){
-          this.router.navigate([homepage]); 
+          this.router.navigate(['/main-page']);
         }
         return v ? false : true;
       })
