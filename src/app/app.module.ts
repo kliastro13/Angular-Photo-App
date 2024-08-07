92128262cb56f9eb93c80ee1a0d49623269b21bd
kliastro13 <it.goncharov@gmail.com>
2024-08-08 00:05:41 +0300

    Commit: #27 replace and move components new photo and categories. Moved categories and new photo components to main page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,8 +19,8 @@ import { WelcomeModule } from './components/welcome/welcome.module';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { AboutEditButtonComponent } from './components/profile/about-edit-button/about-edit-button.component';
 import { AvatarComponent } from './components/profile/avatar/avatar.component';
-import { NewPhotoComponent } from './components/profile/new-photo/new-photo.component';
-import { CategoriesComponent } from './components/profile/categories/categories.component';
+import { NewPhotoComponent } from './components/main-page/new-photo/new-photo.component';
+import { CategoriesComponent } from './components/main-page/categories/categories.component';
 
 
 registerLocaleData(uk);
--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
-  }
+    this.auth.isAuthenticated$.subscribe(v => console.log(v));
+   }
 
 }
